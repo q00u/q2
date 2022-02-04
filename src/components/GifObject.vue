e2e6424e2a21d6640225aa9549d69f3c3f10d075
Phoenix Gabriel <phoenix.gabriel@Verint.com>
2022-02-04 14:41:05 -0800

    Update gif object compunent - Use new gif object state    - Dim self when others hovered    - Dim others when self hovered    - Dim none when none hovered - Add copy to clipboard on click

--- src/components/GifObject.vue
+++ src/components/GifObject.vue
@@ -1,10 +1,21 @@
 <template>
-  <q-img :src="gifSrc" :ratio="1" class="img-responsive" fit="cover" />
+  <q-img
+    :ratio="1"
+    :src="gifSrc"
+    :class="{ dimmed: dimMyself }"
+    class="img-responsive"
+    fit="cover"
+    @mouseover="dimOthers"
+    @mouseleave="undim"
+    @click="copy"
+  />
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue';
+import { useGifStore } from 'src/store/gifobject';
 import giphyApi from 'giphy-api';
+import { copyToClipboard } from 'quasar';
 
 export default defineComponent({
   name: 'GifObject',
@@ -15,6 +26,7 @@ export default defineComponent({
     },
   },
   setup(props) {
+    const gifStore = useGifStore();
     const gifSrc = computed(() => {
       const fixedHeightWidth = parseInt(props.gifObject.images.fixed_height.width, 10);
       if (fixedHeightWidth < 200) {
@@ -23,7 +35,34 @@ export default defineComponent({
       return props.gifObject.images.fixed_height.url;
     });
 
-    return { gifSrc };
+    const copy = () => {
+      copyToClipboard(props.gifObject.url)
+        .then(() => {
+          // Toast success!
+        })
+        .catch(() => {
+          // Toast failure :-(
+        });
+    };
+
+    const dimMyself = computed(() => gifStore.activeIds.length > 0
+      && !gifStore.activeIds.includes(props.gifObject.id));
+
+    const dimOthers = () => {
+      gifStore.activate(props.gifObject.id);
+      // eslint-disable-next-line no-console
+      console.debug('Dim others', props.gifObject.id, JSON.stringify(gifStore.activeIds));
+    };
+
+    const undim = () => {
+      // eslint-disable-next-line no-console
+      console.debug('undim', props.gifObject.id);
+      gifStore.deactivate(props.gifObject.id);
+    };
+
+    return {
+      gifSrc, copy, dimMyself, dimOthers, undim,
+    };
   },
 });
 </script>
