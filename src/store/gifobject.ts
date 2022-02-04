6ea343c4fb46693b5fb8f77c35a110ff59ecc7bd
Phoenix Gabriel <phoenix.gabriel@Verint.com>
2022-02-04 15:18:29 -0800

    Clean up debugs from GifState

--- src/store/gifobject.ts
+++ src/store/gifobject.ts
@@ -1,8 +1,7 @@
-/* eslint-disable no-console */
 import { defineStore } from 'pinia';
 
 export interface GifState {
-  activeIds: string[],
+  activeIds: string[], // Avoid race conditions with array rather than single string
 }
 
 export const useGifStore = defineStore('Gif', {
@@ -13,18 +12,13 @@ export const useGifStore = defineStore('Gif', {
   actions: {
     activate(idString: string) {
       if (!this.activeIds.includes(idString)) this.activeIds.push(idString);
-      // this.activeIds.add(idString);
-      console.debug('GifState: activate', idString, this.activeIds);
     },
     deactivate(idString: string) {
       if (this.activeIds.includes(idString)) {
         this.activeIds.splice(this.activeIds.indexOf(idString), 1);
       }
-      // this.activeIds.delete(idString);
-      console.debug('GifState: deactivate', idString, this.activeIds);
     },
     clear() {
-      // this.activeIds.clear();
       this.activeIds.length = 0;
     },
   },
