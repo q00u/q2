54ff8835000d328c99db90d063c16c0100997589
Phoenix Gabriel <phoenix.gabriel@Verint.com>
2022-02-05 00:28:29 -0800

    Add search history dialog

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -20,6 +20,7 @@
           dense
           standout
           style="width: 80%"
+          @focus="showHistory = true"
           @keydown.enter="runSearch(searchText)"
         >
           <template v-slot:append>
@@ -49,6 +50,35 @@
       </q-toolbar>
     </q-header>
 
+    <q-dialog v-model="showHistory" no-focus no-refocus no-shake seamless>
+      <q-card style="min-width: 50%">
+        <q-card-section class="row items-center no-wrap">
+          <div class="text-h5">Search History</div>
+          <q-space />
+          <q-btn flat round icon="close" v-close-popup />
+        </q-card-section>
+        <q-separator />
+        <q-card-section style="max-height: 50vh" class="scroll">
+          <div class="column">
+            <div
+              v-for="(item, index) in historyList"
+              :key="index"
+              class="col"
+            >
+              <div class="row justify-between">
+                <div class="col">
+                  <q-btn flat @click="runSearch(item)">{{ item }}</q-btn>
+                </div>
+                <div class="col-1">
+                  <q-btn flat round icon="autorenew" @click="runSearch(item, false)" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -57,7 +87,7 @@
 
 <script lang="ts">
 import { useSearchStore } from 'src/store/search';
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'MainLayout',
@@ -66,11 +96,16 @@ export default defineComponent({
     const version = process.env.APP_VERSION;
     const searchStore = useSearchStore();
 
-    // TODO Add search history view
+    // search history view
+    const showHistory = ref(false);
+    const historyList = computed(() => Object.keys(searchStore.searchHistory));
+
     const searchText = ref(searchStore.activeSearch);
     const runSearch = (text:string, cached = true) => {
       // eslint-disable-next-line no-console
       console.debug('searchText', text, cached);
+      // Hide history box on search
+      showHistory.value = false;
       // Run searchStore action
       searchStore.newSearch(text, cached);
       // Update search box
@@ -89,9 +124,11 @@ export default defineComponent({
     };
 
     return {
-      runSearch,
-      searchText,
       version,
+      searchText,
+      runSearch,
+      showHistory,
+      historyList,
       showSettings,
       settings,
     };
